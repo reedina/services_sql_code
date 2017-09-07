8c3df3eb8514964c0f698c3533b89c13d9f0fc0a
Reedina <reedinafarms@gmail.com>
2017-09-07 15:15:33 -0700

    updated the sql for sbm

--- service_build_manager.sql
+++ service_build_manager.sql
@@ -1,7 +1,14 @@
 CREATE TABLE IF NOT EXISTS sbm_environments (
  id serial primary key,
  name varchar(200) unique,
- description varchar(200),
- type varchar(200)
+ description varchar(200)
 );
 
+-- One Environement to Many Environment Types
+
+CREATE TABLE IF NOT EXISTS sbm_environment_types (
+ id serial primary key,
+ name varchar(200),
+ sbm_environment_id int,
+ unique(name, sbm_environment_id)
+);
