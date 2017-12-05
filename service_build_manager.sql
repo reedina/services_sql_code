11a6cd0f3971bd452c3de792093fe2668657fe00
Reedina <reedinafarms@gmail.com>
2017-12-05 14:21:52 -0800

    updated service_build_manager sql code

--- service_build_manager.sql
+++ service_build_manager.sql
@@ -1,11 +1,13 @@
+CREATE TYPE env_type AS ENUM ('qa', 'staging', 'production');
 CREATE TABLE IF NOT EXISTS sbm_environments (
  id serial primary key,
  name varchar(200),
- type varchar(200),
- description varchar(200),
+ type env_type,
+ url varchar(200),
  unique(name,type)
 );
 
+insert into sbm_environments(name, type, url) values ('test01', 'qa', 'http://localhost:8001/api/test');
 
 CREATE TABLE IF NOT EXISTS sbm_environment_instances (
  id serial primary key,
